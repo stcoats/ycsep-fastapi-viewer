d0b233d87bf2b822c871fda7732053ce54fe2f72
stcoats <32770789+stcoats@users.noreply.github.com>
2025-03-20 11:35:07 +0700

    Update duckdb_utils.py

--- app/duckdb_utils.py
+++ app/duckdb_utils.py
@@ -1,10 +1,6 @@
-from huggingface_hub import hf_hub_download
-import duckdb
-import os
-
 import duckdb
 
 def get_connection():
-    return duckdb.connect("/mnt/data/ycsep.duckdb", read_only=True)
+    return duckdb.connect("app/forensic.duckdb")
 
 
