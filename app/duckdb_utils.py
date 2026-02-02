fe6e8b0818d0dde4edbfb0d295baf381d332c75f
stcoats <32770789+stcoats@users.noreply.github.com>
2026-02-02 13:53:52 +0200

    Refactor get_connection to use local database
    Updated get_connection function to connect to a new database path and removed the download logic.

--- app/duckdb_utils.py
+++ app/duckdb_utils.py
@@ -1,15 +1,5 @@
+from huggingface_hub import hf_hub_download
 import duckdb
 import os
-import urllib.request
-
 def get_connection():
-    db_path = "v2_data.db"
-    db_url = "https://a3s.fi/swift/v1/YCSEP_v2/v2_data.db"
-
-    # Download the ~1GB database if it's not already in the container
-    if not os.path.exists(db_path):
-        print(f"Downloading DB from {db_url}...")
-        urllib.request.urlretrieve(db_url, db_path)
-
-    # Connect to the local copy
-    return duckdb.connect(db_path, read_only=True)
+ return duckdb.connect("/mnt/data/ycsep.duckdb", read_only=True)
